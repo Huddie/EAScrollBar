207bb4dc9999fe5bb755dab4241e51237bbdc498
Ehud Adler <easports96@gmail.com>
2018-01-30 18:36:33 -0500

    Initial Commit

---
+++ EAScrollBar/EAScrollBar.h
@@ -0,0 +1,19 @@
+//
+//  EAScrollBar.h
+//  EAScrollBar
+//
+//  Created by Ehud Adler on 1/30/18.
+//  Copyright © 2018 Ehud Adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EAScrollBar.
+FOUNDATION_EXPORT double EAScrollBarVersionNumber;
+
+//! Project version string for EAScrollBar.
+FOUNDATION_EXPORT const unsigned char EAScrollBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EAScrollBar/PublicHeader.h>
+
+
